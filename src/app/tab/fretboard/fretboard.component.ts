69f074cd7485cf39e6745dbaeffd70c5d0b00962
pinto4903 <andrempinto2003@gmail.com>
2024-06-28 17:03:35 +0100

    feat(fretboard): added note synthetizing

--- src/app/tab/fretboard/fretboard.component.ts
+++ src/app/tab/fretboard/fretboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import * as Tone from 'tone';
 
 @Component({
   selector: 'app-fretboard',
@@ -9,12 +10,39 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class FretboardComponent {
   @Output() fretClickEvent = new EventEmitter<[number, number]>();
+  sampler = new Tone.Sampler({
+    urls: {
+      E2: 'low_e.mp3',
+      A2: 'A.mp3',
+      D3: 'D.mp3',
+      G3: 'G.mp3',
+      B3: 'B.mp3',
+      E4: 'high_e.mp3',
+    },
+    baseUrl: 'assets/sounds/',
+  }).toDestination();
 
   onClick(e: Event) {
-    const targetElement = e.target as HTMLElement;
-    let str: string = targetElement.getAttribute('string')!;
-    let fret: string = targetElement.getAttribute('fret')!;
-    console.log(`String ${str} Fret ${fret}`);
-    this.fretClickEvent.emit([parseInt(str), parseInt(fret)]);
+    console.log(e);
+    const targetElement = e.target as Element;
+    const id = targetElement.id;
+    let regex: RegExp = /\d+/g;
+    let matches: string[] = id.match(regex)!;
+    let numbers: number[] = matches.map((match) => parseInt(match, 10));
+    this.fretClickEvent.emit([numbers[0], numbers[1]]);
+
+    if ((e as KeyboardEvent).ctrlKey) {
+      this.sampler.triggerAttackRelease(this.fretToNote(numbers[0], numbers[1]), '2n');
+    }
+  }
+
+  fretToNote(str: number, fret: number): string {
+    const key = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+
+    const num = (6 - str) * 5 + fret - (str < 3 ? 1 : 0);
+    console.log(num);
+    const note = `${key[(num + 7) % 12]}${Math.ceil(2 + Math.max(num - 7, 0) / 12)}`;
+    console.log(note);
+    return note;
   }
 }
